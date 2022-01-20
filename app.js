cc89dbc2a7af313562ad981b854466b64ff1e685
ckd0325 <ckd0325@gmail.com>
2022-01-21 05:53:07 +0900

    로그인 일부 학습

--- app.js
+++ app.js
@@ -1,5 +1,8 @@
 const express = require('express');
 const path = require('path');
+const expressSession = require('express-session');
+const passport = require('./passport.js');
+const flash = require('connect-flash');
 
 const { sequelize } = require('./models');
 
@@ -16,9 +19,29 @@ app.use(express.static(__dirname + "/front_end/view"));
 app.use(express.static(__dirname + "/front_end"));
 app.use(express.json());
 
+app.use(passport.initialize())
+app.use(flash())
+app.use(expressSession({
+    secret: 'my Key',
+    resave: true,
+    saveUninitialized: true
+}));
+app.use(passport.session())
+
 app.get("/", (req, res) => { //첫 연결: 클라이언트에 html 전송
     console.log("success");
     res.sendFile(path.resolve("front_end", "view", "page.html"));
 });
+
+app.post("/login", passport.authenticate('local-login', {
+    successRedirect: '/',
+    failureRedirect: '/login',
+    successFlash: "welcome",
+    failureFlash: true,
+}));
+
+app.get("/login", (req, res) => {
+    console.log("login fail");
+})
 
 app.listen(process.env.PORT || 8080, () => console.log("Server runing ..."));
--- front_end/view/view.js
+++ front_end/view/view.js
@@ -1,19 +1,55 @@
 import { spaInit } from "../controller.js";
+import { $rootContent } from "../model/html_element.js";
 
-(function pageInit() {
-    document.body.innerHTML += `
-    <div class="menu-bar">
-      <div class="menu-bar__left"><img src="menu_button.svg"></div>
-      <div class="menu-bar__center" data-link="">아주대 직방</div>
-      <div class="menu-bar__right" data-link="log-in">Log In</div>
-    </div>
+export function pageInit() {
+    document.body.innerHTML = `
+  <div class="menu-bar">
+    <div class="menu-bar__left"><img src="menu_button.svg"></div>
+    <div class="menu-bar__center" data-link="">아주대 직방</div>
+    <div class="menu-bar__right" data-link="login">Log In</div>
+  </div>
+  <div class="content-box">
     <div class="root-content">
     </div>
-    `;
+  </div>
+  `;
 
     spaInit();
-}());
+};
 
-function drawLoginPage() {
 
+export function drawMainPage() {
+    document.querySelector('.root-content').innerHTML = ``;
+}
+
+export function drawLoginPage() {
+    document.querySelector('.root-content').innerHTML = `
+    <form class="login-signup" action="/login" method="post">
+      <input class="login-signup__input" id="id" type="text" name="id" placeholder="아이디를 입력해주세요"/>
+      <input class="login-signup__input" id="password" type="password" name="password" placeholder="비밀번호를 입력해주세요"/>
+      <input class="login-signup__button" type="button" name="sign-up" value="회원가입" data-link="sign-up"/>
+      <input class="login-signup__button" id="login-button" type="submit" name="log-in" value="로그인"/>
+    </form>
+    `;
+
+}
+
+export function drawSignUpPage() {
+    document.querySelector('.root-content').innerHTML = `
+  <form class="login-signup">
+    <input class="login-signup__input" type="text" name="id" placeholder="아이디를 입력해주세요">
+    <input class="login-signup__input" type="text" name="password" placeholder="비밀번호를 입력해주세요">
+    <input class="login-signup__input" type="text" name="region" placeholder="지역을 입력해주세요">
+    <input class="login-signup__button" type="button" name="cancel" value="취소" data-link="login">
+    <input class="login-signup__button" type="submit" name="sign-up" value="회원가입">
+  </form>
+  `
+}
+
+
+function deleteElementAll() { // root-content의 요소 모두 지움
+    $rootContent = document.querySelector(".root-content");
+    while ($rootContent.hasChildNodes()) {
+        $rootContent.removeChild($rootContent.firstChild);
+    }
 }
